4eda1e6bfaf30dbd7afa16930ff2b4efa91ec4e9
mexxtz <matiexe31@gmail.com>
2021-06-03 16:56:43 -0300

    se agrega funcion que determina el ganador

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
+import './index.css';
+
 
 function Square (props) {
       return (
@@ -35,7 +36,13 @@ function Square (props) {
     }
   
     render() {
-      const status = 'Next player: X';
+      let status;
+      const winner = calculateWinner(this.state.squares);
+      if(winner){
+          status = 'Winner: ' + winner;
+      }else{
+          status = 'Next Player:' + (this.state.xIsNext ? 'X':'O');
+      }
   
       return (
         <div>
@@ -75,6 +82,26 @@ function Square (props) {
       );
     }
   }
+
+  function calculateWinner(squares) {
+    const lines =[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+    for(let i = 0;i<lines.length; i++){
+        const [a,b,c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a];
+        }
+    }
+    return null;
+}
   
   // ========================================
   
